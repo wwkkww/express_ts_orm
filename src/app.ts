e6a2f52819218999bea07995e15be800fe492622
kevin <kevin@betacode.io>
2020-03-12 21:28:27 +0800

    first commit. Create class for app instance

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+class App {
+  public app: express.Application;
+  public port: number;
+
+  constructor(controllers: any, port: number) {
+    this.app = express();
+    this.port = port
+
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers)
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(express.json());
+  }
+
+  private initializeControllers(controllers: any) {
+    controllers.forEach(controller => {
+      this.app.use('/', controller.router)
+    });
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => console.log(`Server started at port${this.port}`))
+  }
+}
+
+export default App;
