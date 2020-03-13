71696a73ca1c120a1d8e09b7240a4aa3cf20f139
kevin <kevin@betacode.io>
2020-03-13 09:13:24 +0800

    add dotenv. setup mongoDB connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
 import App from './app';
 import PostsController from './posts/posts.controller';
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
+
+// connect mongoDB
+mongoose.connect(
+  `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`,
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err))
 
 const app = new App([new PostsController()], 5000)
 
