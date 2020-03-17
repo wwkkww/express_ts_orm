215d7fea79988c3ba79232800f3bf72680488a77
kevin <kevin@betacode.io>
2020-03-18 00:19:08 +0800

    add User entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+class User {
+  @PrimaryGeneratedColumn()
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public password: string;
+
+}
+
+export default User;
